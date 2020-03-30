a99db760a3596d3ffec4330f55910b36ffe77b8d
Derek phung <dragevards@gmail.com>
2020-03-30 23:18:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(array, string)
 end
 
 def now_serving(array)
-  if array = []
+  if array.count == 0
     puts "There is nobody waiting to be served!"
   elsif array.count > 0
     puts "Currently serving #{array[0]}."
